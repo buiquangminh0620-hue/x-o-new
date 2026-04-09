63cb9f472fef136a3789f620274c3c552ef55d06
buiquangminh0620-hue <buiquangminh0620@gmail.com>
2026-04-09 19:28:14 +0700

    Update Main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
 import GUI.MainMenu;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    private static void setGlobalFont(Font font) {
+        for (Object key : UIManager.getDefaults().keySet()) {
+            Object value = UIManager.get(key);
+            if (value instanceof Font) {
+                UIManager.put(key, font);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {
+            }
+
+            setGlobalFont(new Font("SansSerif", Font.PLAIN, 16));
+
             MainMenu ui = new MainMenu();
             ui.setVisible(true);
         });
