0b3e88fe028a4d64b562ff31b0ebb4e0d5283cee
buiquangminh0620-hue <buiquangminh0620@gmail.com>
2026-04-09 14:29:29 +0700

    Create Characters.java

---
+++ src/Fancy/Characters.java
@@ -0,0 +1,11 @@
+package Fancy;
+
+public class Characters {
+    private static final String[] CHARACTERS = {
+            "X", "O", "★", "♥", "☀", "☁", "▲", "●", "◆", "♣", "♠", "☺"
+    };
+
+    public static String[] names() {
+        return CHARACTERS;
+    }
+}
