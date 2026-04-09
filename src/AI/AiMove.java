5558253ea77225f23e9089ff5af82e2ffe4b0c4a
buiquangminh0620-hue <buiquangminh0620@gmail.com>
2026-04-09 21:10:20 +0700

    Create AiMove.java for AI package

---
+++ src/AI/AiMove.java
@@ -0,0 +1,11 @@
+package AI;
+
+public class AiMove {
+    public final int row;
+    public final int col;
+
+    public AiMove(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
